b0334616b03a0b9f0f238ff5a7ed239634d55646
Ariefianto Rizky <ariefiantorizky2907@gmail.com>
2021-10-23 11:52:16 +0700

    Updating content

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,3 +3,17 @@ const allImage = document.querySelectorAll("img");
 for (let i = 0; i < allImage.length; i++) {
     allImage[i].setAttribute("loading", "lazy");
 }
+
+CSS.registerProperty({
+    name: "--p",
+    syntax: "<integer>",
+    initialValue: 0,
+    inherits: true,
+})
+
+window.addEventListener("load", () => {
+    setInterval(() => {
+        document.querySelector(".loading").style.display = "none";
+        document.body.classList.remove("content-loaded");
+    }, 1000);
+})
